25d4dd5f6f588ee91c6455e86fc17a5ed7345944
bautigaraventa <bautistagaraventa@hotmail.com>
2020-09-17 15:16:02 -0300

    initial commit: api rest configuration. Basic architecture created. Main routes (endpoints) created.

---
+++ src/routes/MessageRoutes.ts
@@ -0,0 +1,35 @@
+import * as express from "express";
+import { messageController } from "../controllers/MessageController";
+import { messageValidator } from '../validators/MessageValidators';
+
+class MessageRoutes {
+    private baseUrl = '/messages';
+    public router: express.Router = express.Router();
+
+    constructor() {
+        this.config();
+    }
+
+    private config(): void {
+
+        // comment later
+        this.router.get(
+            `${this.baseUrl}`,
+            (req: express.Request, res: express.Response) => messageController.receive(req, res)
+        )
+
+        // comment later
+        this.router.post(
+            `${this.baseUrl}`,
+            messageValidator.send,
+            (req: express.Request, res: express.Response) => messageController.send(req, res));
+
+        // comment later
+        this.router.put(
+            `${this.baseUrl}/:messageId/`,
+            messageValidator.markAsProcessed,
+            (req: express.Request, res: express.Response) => messageController.markAsProcessed(req, res));
+    }
+}
+
+export const messageRoutes = new MessageRoutes().router;
